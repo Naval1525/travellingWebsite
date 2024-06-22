6eb63e5ba804fa86a7db580b2e54d26740c8ec7a
Naval Bihani <144666232+Naval1525@users.noreply.github.com>
2024-06-22 19:22:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+let searchForm = document.querySelector('.search-form');
+
+const hi = function(){
+  searchForm.classList.add('active');
+}
+const bye = function(){
+  searchForm.classList.remove('active');
+}
